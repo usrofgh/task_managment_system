abfb0987f033e137e8648b584eace13832cdc59d
usrofgh <mailforgimail@gmail.com>
2023-03-30 15:38:07 +0300

    Add index view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from todo.models import Worker, Task
+
+
+def index(request):
+    worker_count = Worker.objects.count()
+    to_do_count = Task.objects.filter(completing_step=0).count()
+    doing_count = Task.objects.filter(completing_step=1).count()
+    done_count = Task.objects.filter(completing_step=2).count()
+
+    context = {
+        "worker_count": worker_count,
+        "to_do_count": to_do_count,
+        "doing_count": doing_count,
+        "done_count": done_count,
+    }
+    return render(request, "todo/index.html", context=context)
