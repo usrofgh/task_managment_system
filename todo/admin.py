709c13b9b31f52dd20cae85ec393ea65ef86e3f0
usrofgh <mailforgimail@gmail.com>
2023-03-30 16:20:57 +0300

    Add Admin view

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from todo.models import TaskType, Position, Worker, Task
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = ("username", "first_name", "last_name", "position",
+                    "email", "is_active", "is_staff")
+    fieldsets = (
+        (
+            None,
+            {"fields": ("username", "password")}
+        ),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email", "position")}
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}
+        )
+    )
+
+    search_fields = ["username", "first_name", "last_name", "email"]
+    ordering = ["username", "last_name", "first_name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    ordering = ["name"]
