d406b2a7cbb33a525b2c385a37dd48d30f6b46ff
usrofgh <mailforgimail@gmail.com>
2023-03-30 15:39:02 +0300

    Add path in global urls

--- task_manager_system/urls.py
+++ task_manager_system/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from todo.views import index
+
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
+    path("", index)
 ]
