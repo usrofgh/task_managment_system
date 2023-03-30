a867a7f672394c074bb25431a30096c4d0ba17a2
usrofgh <mailforgimail@gmail.com>
2023-03-30 15:46:28 +0300

    Add for position: html files, urls, view(list, detail, update, delete)

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,16 @@
-urlpatterns = [
+from todo.views import (
+    PositionListView,
+    PositionDetailView,
+    PositionUpdateView,
+    PositionDeleteView,
+)
+from django.urls import path
 
+urlpatterns = [
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/<int:pk>/", PositionDetailView.as_view(), name="position-detail"),
+    path("positions/<int:pk>/edit/", PositionUpdateView.as_view(), name="position-edit"),
+    path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-from todo.models import Worker, Task
+from todo.models import Worker, Task, Position
 
 
 def index(request):
@@ -17,3 +19,21 @@ def index(request):
         "done_count": done_count,
     }
     return render(request, "todo/index.html", context=context)
+
+
+class PositionListView(generic.ListView):
+    model = Position
+
+
+class PositionDetailView(generic.DetailView):
+    model = Position
+
+
+class PositionUpdateView(generic.UpdateView):
+    model = Position
+    success_url = reverse_lazy("catalog:position-list")
+
+
+class PositionDeleteView(generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("catalog:position-list")
