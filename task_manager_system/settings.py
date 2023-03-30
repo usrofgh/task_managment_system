9d0ed70360fbbc78967bcf40210b1d443d3f5153
usrofgh <mailforgimail@gmail.com>
2023-03-30 16:04:01 +0300

    Add crispy_forms & crispy_bootstrap5

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "debug_toolbar",
     "todo",
 ]
