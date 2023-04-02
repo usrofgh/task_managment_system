c1f637c7a84fbdeefd6116338502c0bee56d8275
usrofgh <mailforgimail@gmail.com>
2023-04-02 11:57:01 +0300

    Remove insecure django secret_key from settings.py

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -26,7 +26,7 @@ LOGIN_REDIRECT_URL = "/"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%^-^frf@)--(ykw1tg2q+d5@1#aq=r=j-9vugh0(2jt4+8)2a7")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
